f5e7b30131454a4cc60421a4a75dd6a39767c424
Srinivaslancius <srinivas@lanciussolutions.com>
2017-08-21 18:36:47 +0530

    serfvice details

--- index.php
+++ index.php
@@ -285,7 +285,7 @@
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <h3><?php echo $getAllData1['title']; ?></h3>
                             <?php echo $getAllData1['description']; ?>
-                            <a href="service_details.php" class="btn-hover-ghost-accent">visit service</a>
+                            <a href="service_details.php?id=<?php echo $getAllData1['id']; ?>" class="btn-hover-ghost-accent">visit service</a>
                         </div>  
                     </div> 
                     <?php $i++; } ?>                 
