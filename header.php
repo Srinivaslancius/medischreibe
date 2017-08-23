98a4d133443f1e3064665966d8046ee5e1c8fa07
srinivaslancuius <srinivas@lanciussolutions.com>
2017-08-23 12:23:49 +0530

    changes in content_pages, header file, contact_us files

--- header.php
+++ header.php
@@ -1,4 +1,10 @@
 <?php $getMenus = getAllDataCheckActive('services',0); ?>
+
+<?php
+    $currentFile = $_SERVER["PHP_SELF"];
+    $parts = Explode('/', $currentFile);
+    $page_name = $parts[count($parts) - 1];
+?>
 <div class="header2-area">
                     <div class="header-top-area">
                         <div class="container">
@@ -30,14 +36,15 @@
                                     <div class="logo-area">
                                         <a href="index.php"><img class="img-responsive" src="img/logo.png" alt="logo"></a>
                                     </div>
-                                </div>  
+                                </div> 
+
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                     <div class="main-menu-area">
                                         <nav>
                                             <ul>
-                                                <li><a href="index.php">Home</a></li>
-                                                <li><a href="about_us.php">About Us</a></li>
-                                                <li><a href="#">Services</a>
+                                                <li <?php if($page_name == 'index.php') {  ?>class="active" <?php } ?>> <a href="index.php">Home</a></li>
+                                                <li <?php if($page_name == 'about_us.php') {  ?>class="active" <?php } ?>> <a href="about_us.php">About Us</a></li>
+                                                <li <?php if($page_name == 'service_details.php') {  ?>class="active" <?php } ?>><a href="service_details.php">Services</a>
                                                     <ul>
                                                     <?php while($getAllMenus = $getMenus->fetch_assoc()) { ?>
                                                         <li><a href="service_details.php?id=<?php echo $getAllMenus['id']; ?>"><?php echo $getAllMenus['title']; ?></a></li>
@@ -45,7 +52,7 @@
                                                     </ul>
                                                 </li>
                                                 
-                                                <li><a href="contact_us.php">Contact</a></li>
+                                                <li <?php if($page_name == 'contact_us.php') {  ?>class="active" <?php } ?>><a href="contact_us.php">Contact</a></li>
                                             </ul>
                                         </nav>
                                     </div>
@@ -85,3 +92,25 @@
                         </div>
                     </div>
                 </div>
+
+
+
+
+
+
+              <!--   <ul class="menu">
+                    <li <?php if($page_name == 'index.php') {  ?>class="current-menu-item" <?php } ?>><a href="index.php">Home</a></li>
+                    <li <?php if($page_name == 'about_us.php') {  ?>class="current-menu-item" <?php } ?>>
+                        <a href="about_us.php">About Us</a>
+                    </li>
+                    <li <?php if($page_name == 'courses.php') {  ?>class="current-menu-item" <?php } ?>>
+                        <a href="courses.php">Courses</a>
+                        
+                   </li>
+                    <li <?php if($page_name == 'student_verification.php') {  ?>class="current-menu-item" <?php } ?>>
+                        <a href="student_verification.php">Student Verification</a>
+                    </li>
+                    <li <?php if($page_name == 'contact_us.php') {  ?>class="current-menu-item" <?php } ?>>
+                        <a href="contact_us.php">Contact Us</a>
+                    </li>
+                </ul>  -->
