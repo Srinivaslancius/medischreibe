fc1481677554eb900b83ad44637a769e283d62ea
Gunavardhan <vardhan.gujjula@gmail.com>
2017-08-23 12:45:39 +0530

    changes in main header

--- main_header.php
+++ main_header.php
@@ -4,13 +4,29 @@ include "admin/includes/config.php";
 include "admin/includes/functions.php"; 
 $getSiteSettingsData = getIndividualDetails('1',"site_settings","id"); 
 ?>
-<!doctype html>
+<?php 
+    $currentFile = $_SERVER["PHP_SELF"];
+    $parts = Explode('/', $currentFile);
+    $page_name = $parts[count($parts) - 1];
+?>
+<?php
+    if($page_name == 'about_us.php'){
+        $getMetaData = getIndividualDetails(4,"content_pages","id");
+    }
+     else{
+       $id= isset($_GET['id']); 
+       $getMetaData = getIndividualDetails($id,"services","id");
+    }
+?>
+
 <html class="no-js" lang="">
 <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title><?php echo $getSiteSettingsData['admin_title']; ?></title>
-        <meta name="description" content="">
+        <meta name="title" content="<?php echo $getMetaData['meta_title'] ?>"></meta>
+        <meta name="description" content="<?php echo $getMetaData['meta_desc'] ?>"></meta>
+        <meta name="keywords" content="<?php echo $getMetaData['meta_keywords'] ?>"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Favicon -->
